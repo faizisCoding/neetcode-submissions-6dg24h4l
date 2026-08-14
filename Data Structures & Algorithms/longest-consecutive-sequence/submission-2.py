eb432b07c0c739bb6e9a51a2f43d8a5b1e82de1f
faizisCoding <faiz.gm12@gmail.com>
2026-08-14 21:38:09 +0530

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        map = {}
+        for i in nums:
+            map[i]=0
+        for num in map.keys():
+            if num-1 in map.keys():
+                continue
+            i=num
+            count=1
+            while i+1 in map.keys():
+                count+=1
+                i+=1
+            map[num]=count
+        print(map)
+        return max(map.values())
+            
+        
+        
